21552bda4642e2a53883ca0e6800700b31f7132e
kash-pram <17083388+kash-pram@users.noreply.github.com>
2025-11-01 04:24:19 +0530

    updated env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseKey: 'YOUR_SUPABASE_ANON_KEY',
-  apiUrl: 'YOUR_RENDER_API_URL'
+  supabaseUrl: 'https://nvhmlpdrvmgxwtgddzyh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52aG1scGRydm1neHd0Z2RkenloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTM4NTIsImV4cCI6MjA3NzQ2OTg1Mn0.LB6JyzMWk_D5XJNDpfiuqlxvgunb_zS0TB7uY-azy84',
+  apiUrl: 'https://pin-vaasal-kadhavu-src.onrender.com'
 };
